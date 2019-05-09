0f472a08337bf9bbdbf4d4814e7dde57b74c4e35
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-05-08 17:21:18 -0700

    playing with styling

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -4,16 +4,21 @@ import './App.css';
 import {Sidebar} from "./containers/Sidebar";
 import {MessagesList} from "./containers/MessagesList";
 import {AddMessage} from "./containers/AddMessage";
+import NavBar from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
+      <div>
+      <NavBar />   
       <div id="container">
+
        <Sidebar />
         <section id="main">
          <MessagesList />
          <AddMessage />
         </section>
+      </div>
       </div>
     );
   }
